93bd3c804204a049a6d9f56f9f2f6c5decfcdd2e
William Woodall <william@osrfoundation.org>
2014-08-11 14:15:31 -0700

    cli_utils: documentation for verb_pattern

--- osrf_pycommon/cli_utils/verb_pattern.py
+++ osrf_pycommon/cli_utils/verb_pattern.py
@@ -12,11 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""API for implementing commands and verbs which used the verb pattern."""
+
 import pkg_resources
 import inspect
 
 
 def call_prepare_arguments(func, parser, sysargs=None):
+    """Call a prepare_arguments function with the correct number of parameters.
+
+    The ``prepare_arguments`` function of a verb can either take one parameter,
+    ``parser``, or two parameters ``parser`` and ``args``, where ``args`` are
+    the current arguments being processed.
+
+    :param func: Callable ``prepare_arguments`` function.
+    :type func: Callabe
+    :param parser: parser which is always passed to the function
+    :type parser: :py:class:`argparse.ArgumentParser`
+    :param sysargs: arguments to optionally pass to the function, if needed
+    :type sysargs: list
+    :returns: return value of function or the parser if the function
+        returns None.
+    :rtype: :py:class:`argparse.ArgumentParser`
+    """
     func_args = [parser]
     # If the provided function takes two arguments and args were given
     # also give the args to the function
@@ -30,6 +48,30 @@ def call_prepare_arguments(func, parser, sysargs=None):
 
 
 def create_subparsers(parser, cmd_name, verbs, group, sysargs, title=None):
+    """Creates argparse subparsers for each verb which can be discovered.
+
+    Using the ``verbs`` parameter, the availble verbs are iterated through.
+    For each verb a subparser is created for it using the ``parser`` parameter.
+    The ``cmd_name`` is used to fill the title and description of the
+    ``add_subparsers`` function call.
+    The ``group`` parameter is used with each verb to load the verb's
+    ``description``, ``prepare_arguments`` function, and the verb's
+    ``argument_preprocessors`` if available.
+    Each verb's ``prepare_arguments`` function is called, allowing them to
+    add arguments.
+    Finally a list of ``argument_preprocessors`` functions and verb subparsers
+    are returned, one for each verb.
+
+    :param parser: parser for this command
+    :type parser: :py:class:`argparse.ArgumentParser`
+    :param str cmd_name: name of the command to which the verbs are being added
+    :param list verbs: list of verbs (by name as a string)
+    :param str group: name of the ``entry_point`` group for the verbs
+    :param list sysargs: list of system arguments
+    :param str title: optional custom title for the command
+    :returns: tuple of argument_preprocessors and verb subparsers
+    :rtype: tuple
+    """
     metavar = '[' + ' | '.join(verbs) + ']'
     subparser = parser.add_subparsers(
         title=title or '{0} command'.format(cmd_name),
@@ -64,11 +106,18 @@ def create_subparsers(parser, cmd_name, verbs, group, sysargs, title=None):
 
 
 def default_argument_preprocessor(args):
+    """Return unmodifed args and an empty dict for extras"""
     extras = {}
     return args, extras
 
 
 def list_verbs(group):
+    """List verbs available for a given ``entry_point`` group.
+
+    :param str group: ``entry_point`` group name for the verbs to list
+    :returns: list of verb names for the given ``entry_point`` group
+    :rtype: list of str
+    """
     verbs = []
     for entry_point in pkg_resources.iter_entry_points(group=group):
         verbs.append(entry_point.name)
@@ -76,12 +125,36 @@ def list_verbs(group):
 
 
 def load_verb_description(verb_name, group):
+    """Load description of a verb in a given group by name.
+
+    :param str verb_name: name of the verb to load, as a string
+    :param str group: ``entry_point`` group name which the verb is in
+    :returns: verb description
+    :rtype: dict
+    """
     for entry_point in pkg_resources.iter_entry_points(group=group):
         if entry_point.name == verb_name:
             return entry_point.load()
 
 
 def split_arguments_by_verb(arguments):
+    """Split arguments by verb.
+
+    Given a list of arguments (list of strings), the pre verb arguments,
+    the verb, and the post verb arugments are returned.
+
+    For example:
+
+    .. code-block:: python
+
+        >>> args = ['--command-arg1', 'verb', '--verb-arg1', '--verb-arg2']
+        >>> split_arguments_by_verb(args)
+        (['--command-arg1'], 'verb', ['--verb-arg1', '--verb-arg2'])
+
+    :param list arguments: list of system arguments
+    :returns: pre verb args (list), the verb (str), and post verb args (list)
+    :rtype: tuple
+    """
     verb = None
     pre_verb_args = []
     post_verb_args = []
