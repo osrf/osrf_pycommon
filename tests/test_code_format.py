655b90446ce3971b60e8d3ce891068792d1bc03a
William Woodall <william@osrfoundation.org>
2016-07-29 14:16:33 -0700

    work around hanging issue on Windows

--- tests/test_code_format.py
+++ tests/test_code_format.py
@@ -8,6 +8,8 @@ def test_flake8():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     source_dir = os.path.join(this_dir, '..', 'osrf_pycommon')
     cmd = ['flake8', source_dir, '--count']
+    # work around for https://gitlab.com/pycqa/flake8/issues/179
+    cmd.extend(['--jobs', '1'])
     if sys.version_info < (3,4):
         # Unless Python3, skip files with new syntax, like `yield from`
         cmd.append('--exclude=*async_execute_process_asyncio/impl.py')
