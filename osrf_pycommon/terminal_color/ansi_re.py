f05df24b66f3a79b73380462da404d3a68c3e06f
Chris Lalancette <clalancette@openrobotics.org>
2019-08-27 07:49:49 -0400

    ansi_escape_senquences -> ansi_escape_sequences (#53)
    * ansi_escape_senquences -> ansi_escape_sequences
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Add backwards compatibility.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * And fix the alias so it gets imported as expected.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- osrf_pycommon/terminal_color/ansi_re.py
+++ osrf_pycommon/terminal_color/ansi_re.py
@@ -38,9 +38,13 @@ def split_by_ansi_escape_sequence(string, include_delimiters=False):
     return _ansi_re.split(string)
 
 
-def remove_ansi_escape_senquences(string):
+def remove_ansi_escape_sequences(string):
     """
     Removes any ansi escape sequences found in the given string and returns it.
     """
     global _ansi_re
     return _ansi_re.sub('', string)
+
+
+# Backwards compatibility
+remove_ansi_escape_senquences = remove_ansi_escape_sequences
